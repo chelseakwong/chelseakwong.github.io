f583629476e06bc2c5c8508e2cc10226ba8622e4
Chelsea Kwong <chelseakct@gmail.com>
2017-05-08 04:36:25 -0400

    cleaned up unnecessary files

--- js/headerAnimation.js
+++
@@ -1,9 +0,0 @@
-var cbpAnimatedHeader = (function() {
-    $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('.cbp-af-header').addClass('cbp-af-header-shrink');
-  } else {
-    $('.cbp-af-header').removeClass('cbp-af-header-shrink');
-  }
-});
-})();
